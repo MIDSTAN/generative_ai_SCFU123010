6fe8928e416fff7486f65593b0fac3b90a09d6ad
MIDSTAN <pruthvirajsapate123@gmail.com>
2026-08-17 05:41:06 -0400

    Added activity 9

---
+++ activity_9.py
@@ -0,0 +1,57 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+api_key = os.getenv("GROQ_API_KEY")
+
+client = Groq(
+    api_key=api_key,
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Hi which model are you using",
+        }
+    ],
+    model="openai/gpt-oss-20b",
+)
+
+print(chat_completion.choices[0].message.content)
+
+history = []
+
+while True:
+    prompt = input("Ask Anything: ")
+
+    # Add user's message to history
+    history.append({
+        "role": "user",
+        "content": prompt,
+    })
+
+    chat_completion = client.chat.completions.create(
+        messages=history,
+        model="openai/gpt-oss-20b",
+    )
+
+    response = chat_completion.choices[0].message.content
+
+    # Add AI response to history
+    history.append({
+        "role": "assistant",
+        "content": response,
+    })
+
+    print(response)
+
+    end = input("Continue? (True/False): ").lower()
+
+    if end == "false":
+        break
+
+
+print(history)
